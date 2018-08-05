b17906dc716b8d0d6bf569fe9110f365776d2d16
Alessandro Bonometti <a.bonometti@gmail.com>
2018-08-05 23:19:18 +0200

    First Build with fake5 (still MSBuild)

--- FSharp/build.fsx
+++ FSharp/build.fsx
@@ -1,9 +1,16 @@
-// include Fake libs
-#r "./packages/FAKE/tools/FakeLib.dll"
+// #r "paket:
+// nuget Fake.Core.Target
+// nuget Fake.IO.FileSystem
+// nuget Fake.DotNet
+// nuget Fake.DotNet.MSBuild //"
+#load "./.fake/build.fsx/intellisense.fsx"
 
-open Fake
-open Fake.Testing
-open Fake.AssemblyInfoFile
+open Fake.Core
+open Fake.IO
+open Fake.DotNet
+open Fake.Core.TargetOperators
+open Fake.IO.Globbing.Operators
+// open Fake.Testing
 
 // Directories
 let buildDir  = "./build/"
@@ -19,35 +26,41 @@ let appReferences  =
 let version = "0.1"  // or retrieve from CI server
 
 // Targets
-Target "Clean" (fun _ ->
-    CleanDirs [buildDir; deployDir]
+Fake.Core.Target.create "Clean" (fun _ ->
+    Fake.IO.Shell.cleanDirs [buildDir; deployDir]
 )
 
-Target "Build" (fun _ ->
-    CreateFSharpAssemblyInfo "./Core/Properties/AssemblyInfo.fs"
-        [Attribute.InternalsVisibleTo "Tests" ]
-    !! "/**/*.fsproj"
-    // compile all projects below src/app/
-    |> MSBuildDebug buildDir "Build"
-    |> Log "AppBuild-Output: "
+// Fake.Core.Target.create "Build" (fun _ ->
+//     Fake.DotNet.MSBuild.AssemblyInfoFile.createFSharp "./Core/Properties/AssemblyInfo.fs"
+//         [Fake.DotNet.AssemblyInfo.InternalsVisibleTo "Tests" ]
+//     !! "/**/*.fsproj"
+//     // compile all projects below src/app/
+//     |> Fake.DotNet.MSBuild.runDebug id buildDir "Build"
+//     |> Fake.Core.Trace.logItems "AppBuild-Output: "
+// )
+
+Fake.Core.Target.create "Build" (fun _ ->
+    !! "./**/*.fsproj"
+    |> Fake.DotNet.MSBuild.runDebug id buildDir "Build"
+    |> Fake.Core.Trace.logItems "AppBuild-Output: "
 )
 
-Target "Test" (fun _ ->
-    !! "/**/build/Tests.dll"
-    |> NUnit3 (id)
-)
+// Fake.Core.Target.create "Test" (fun _ ->
+//     !! "/**/build/Tests.dll"
+//     |> Fake.DotNet.Testing.NUnit3.NUnit3Defaults (id)
+// )
 
-Target "Deploy" (fun _ ->
-    !! (buildDir + "/**/*.*")
-        -- "*.zip"
-        |> Zip buildDir (deployDir + "ApplicationName." + version + ".zip")
-)
+// Fake.Core.Target.create "Deploy" (fun _ ->
+//     !! (buildDir + "/**/*.*")
+//         -- "*.zip"
+//         |> Fake.IO.Zip.createZip buildDir (deployDir + "ApplicationName." + version + ".zip")
+// )
 
 // Build order
 "Clean"
   ==> "Build"
-  ==> "Test"
-  ==> "Deploy"
+//   ==> "Test"
+//   ==> "Deploy"
 
 // start build
-RunTargetOrDefault "Test"
+Fake.Core.Target.runOrDefault "Build"
