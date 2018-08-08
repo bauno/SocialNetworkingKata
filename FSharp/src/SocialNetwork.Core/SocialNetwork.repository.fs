5f93e06b54f323ee83610e9b6d9a8158983d3ec5
Alessandro Bonometti <alessandro.bonometti@massive.co>
2018-08-08 17:55:01 +0200

    Data and repository

---
+++ FSharp/src/SocialNetwork.Core/SocialNetwork.repository.fs
@@ -0,0 +1,19 @@
+module internal SocialNetwork.Repository
+
+open System.Collections.Generic
+
+open SocialNetwork.Data
+
+let private data = new Dictionary<User, Wall>()
+
+let save (wall: Wall) =    
+    if data.ContainsKey(wall.User)
+        then data.[wall.User] <- wall                          
+    else data.Add(wall.User, wall)
+
+let loadOrCreateWallOf user = 
+    if data.ContainsKey(user)
+    then data.[user]
+    else {User = user; Follows = list.Empty; Posts = list.Empty}
+
+let internal clear () = data.Clear()
