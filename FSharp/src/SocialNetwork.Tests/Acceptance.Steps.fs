6ececfdd8ff252f548bebad9a18e212a81b897a1
Alessandro Bonometti <alessandro.bonometti@massive.co>
2018-08-08 18:58:01 +0200

    Writing tests

---
+++ FSharp/src/SocialNetwork.Tests/Acceptance.Steps.fs
@@ -0,0 +1,60 @@
+module Acceptance.Steps
+
+open System
+open System.Collections.Generic
+open System.Linq
+open SocialNetwork.Core
+open NUnit.Framework
+open FsUnit
+
+let mutable lines = new List<string>()
+let mutable index = 0
+
+let fakeDisplay line = 
+    lines.Add(line)
+
+let mutable enter = init' fakeDisplay
+
+    
+let ``Given`` (user) continuation =
+    continuation (user)
+
+let ``And`` (user) continuation = 
+    continuation(user)    
+
+let ``posted`` (user) message continuation =
+    continuation (user,message)
+
+let ``to the wall`` (user,message) minutes continuation =
+    continuation(user,message,minutes)
+
+let ``seconds ago`` (user, message, seconds) =
+    let now = DateTime.Now
+    let delta = (float)seconds
+    let postTs = now.AddSeconds(-delta)
+    TimeService.testNow <- Some postTs
+    enter (sprintf "%s -> %s" user message )
+    TimeService.testNow <- Some now
+
+let ``minutes ago``(user,message,minutes) =
+  let now = DateTime.Now
+  let delta = (float)minutes
+  let postTs = now.AddMinutes(-delta)
+  TimeService.testNow <- Some postTs
+  enter (sprintf "%s -> %s" user message )
+  TimeService.testNow <- Some now
+
+let ``follows``(user) followed = 
+    let cmd = sprintf "%s follows %s" user followed
+    enter cmd
+
+let ``When I enter``(user) =
+    enter user    
+
+let ``Then I can read`` displayedMessage =     
+    lines.[index] |> should equal displayedMessage
+    index <- index + 1
+
+let ``And I can read`` displayedMessage =     
+    lines.[index] |> should equal displayedMessage
+    index <- index + 1
---
+++ FSharp/src/SocialNetwork.Tests/acceptanceTests.fs
@@ -0,0 +1,48 @@
+module AcceptanceTests
+
+open NUnit.Framework
+open Acceptance.Steps
+
+[<SetUp>]
+let init() =     
+    lines.Clear()
+    index <- 0
+    SocialNetwork.Repository.clear()    
+
+[<Test>]
+let ``I Can read Alice's posts``() =
+    ``Given`` "Alice" ``posted`` "I love the weather today" ``to the wall`` 5 ``minutes ago`` 
+    ``When I enter`` "Alice" 
+    ``Then I can read`` "I love the weather today (5 minutes ago)"
+
+[<Test>]
+let ``I Can read Bob's posts``() =
+    ``Given`` "Bob" ``posted`` "Damn! We lost!" ``to the wall`` 2 ``minutes ago`` 
+    ``And`` "Bob" ``posted`` "Good game though" ``to the wall`` 1 ``minutes ago``     
+    ``When I enter`` "Bob" 
+    ``Then I can read`` "Good game though (1 minutes ago)"
+    ``And I can read`` "Damn! We lost! (2 minutes ago)"
+
+[<Test>]
+let ``I Can subscribe to a user's posts``() =
+    ``Given`` "Alice" ``posted`` "I love the weather today" ``to the wall`` 5 ``minutes ago`` 
+    ``And`` "Charlie" ``posted`` "I'm in New York today! Anyone wants to have a coffee?" ``to the wall`` 2 ``seconds ago``     
+    ``And`` "Charlie" ``follows`` "Alice"
+    ``When I enter`` "Charlie wall" 
+    ``Then I can read`` "Charlie - I'm in New York today! Anyone wants to have a coffee? (2 seconds ago)"
+    ``And I can read`` "Alice - I love the weather today (5 minutes ago)"
+
+
+[<Test>]
+let ``I Can subscribe to a multiple users' posts``() =
+    ``Given`` "Bob" ``posted`` "Damn! We lost!" ``to the wall`` 2 ``minutes ago`` 
+    ``And`` "Bob" ``posted`` "Good game though" ``to the wall`` 1 ``minutes ago``     
+    ``And`` "Alice" ``posted`` "I love the weather today" ``to the wall`` 15 ``minutes ago`` 
+    ``And`` "Charlie" ``posted`` "I'm in New York today! Anyone wants to have a coffee?" ``to the wall`` 15 ``seconds ago``     
+    ``And`` "Charlie" ``follows`` "Alice"
+    ``And`` "Charlie" ``follows`` "Bob"
+    ``When I enter`` "Charlie wall" 
+    ``Then I can read`` "Charlie - I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)"
+    ``And I can read`` "Bob - Good game though (1 minutes ago)"
+    ``And I can read`` "Bob - Damn! We lost! (2 minutes ago)"
+    ``And I can read`` "Alice - I love the weather today (15 minutes ago)"
