ad275517570f0b62a2c8a1992035f65b47368191
Alessandro Bonometti <alessandro.bonometti@massive.co>
2018-08-10 12:12:38 +0200

    Build works (no tests...yet)

--- CSharp/src/app/SocialNetwork.Main/Program.cs
+++ CSharp/src/app/SocialNetwork.Main/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Reflection;
+using Autofac;
+using CSharp.Core.Factories.Interfaces;
+using CSharp.Core.Services;
+using CSharp.Core.Services.Interfaces;
+using static System.Console;
 
-namespace SocialNetwork.Main
+namespace CSharp.Console
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void RunSocialNetwork(IConsoleSocialNetwork socialNetwork)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Write("Enter command (or 'q' to quit): ");
+                var cmdStr = ReadLine();
+                if (cmdStr == "q") return;                
+                socialNetwork.Enter(cmdStr)
+                    .IfSome(err => PrintError(err));
+            }
+        }
+
+        private static void PrintError(string error)
+        {
+            var currentColor = ForegroundColor;
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("Error: {0}", error);
+            ForegroundColor = currentColor;
+        }
+        
+        
+        public static void Main(string[] args)
+        {                       
+            RunSocialNetwork(InitSocialNetwork());
+        }
+
+        private static IConsoleSocialNetwork InitSocialNetwork()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterAssemblyTypes(Assembly.GetAssembly(typeof(CommandFactory)))
+                .AsImplementedInterfaces();            
+            
+            return builder
+                .Build().Resolve<IConsoleSocialNetwork>();
         }
     }
 }
