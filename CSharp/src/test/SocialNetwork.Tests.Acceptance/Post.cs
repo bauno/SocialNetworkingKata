8f6a3475fcbf82be08fefdc36d27b00fb3d4d694
Alessandro Bonometti <alessandro.bonometti@massive.co>
2018-08-13 12:29:13 +0200

    All ported!

--- CSharp/src/test/SocialNetwork.Tests.Acceptance/Post.cs
+++ CSharp/src/test/SocialNetwork.Tests.Acceptance/Post.cs
@@ -111,43 +111,42 @@ namespace SocialNetwork.Tests.Acceptance
 
             "And he can read 'Alice - I love the weather today (5 minutes ago)'"
             .x(() => CheckMessage("Alice - I love the weather today (5 minutes ago)"));
-
         }
 
         [Scenario]
         public void Multiple_Subscription(){
             "Given Bob posted 'Damn! We lost' to his wall 2 minutes ago"
-            .x(() => {});
+            .x(() => PostToWall("Bob", "Damn! We lost!", 2, "minutes"));
 
-            "And Bob posted 'Good game though' to his wall 1 minutes ago"
-            .x(() => {});
+            "And Bob posted 'Good game though' to his wall 1 minute ago"
+            .x(() => PostToWall("Bob", "Good game though", 1, "minute"));
 
             "And Alice posted 'I love the weather today' to her wall 15 minutes ago"
-            .x(() => {});
+            .x(() => PostToWall("Alice", "I love the weather today", 15, "minutes"));
 
             "And Charlie posted 'I'm in New York today! Anyone wants to have a coffee?' to his wall 15 seconds ago"
-            .x(() => {});
+            .x(() => PostToWall("Charlie", "I'm in New York today! Anyone wants to have a coffee?", 15, "seconds" ));
 
             "And Charlie follows Alice"
-            .x(() => {});
+            .x(() => Follows("Charlie", "Alice"));
 
             "And Charlie follows Bob"
-            .x(() => {});
+            .x(() => Follows("Charlie", "Bob"));
 
             "When someone enters the command 'Charlie wall'"
-            .x(() => {});
+            .x(() => _socialNetwork.Enter("Charlie wall"));
 
             "Then he can read 'Charlie - I''m in New York today! Anyone wants to have a coffee? (15 seconds ago)'"
-            .x(() => {});
+            .x(() => CheckMessage("Charlie - I'm in New York today! Anyone wants to have a coffee? (15 seconds ago)"));
 
             "And he can read 'Bob - Good game though (1 minutes ago)'"
-            .x(() => {});
+            .x(() => CheckMessage("Bob - Good game though (1 minutes ago)"));
 
             "And he can read 'Bob - Damn! We lost (2 minutes ago)'"
-            .x(() => {});
+            .x(() => CheckMessage("Bob - Damn! We lost! (2 minutes ago)"));
 
             "And he can read 'Alice - I love the weather today (15 minutes ago)'"
-            .x(() => {});
+            .x(() => CheckMessage("Alice - I love the weather today (15 minutes ago)"));
 
         }
     }
