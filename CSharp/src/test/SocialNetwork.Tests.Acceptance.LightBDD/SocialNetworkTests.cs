b4d770f993cfc43340c86cb76dca97933b07b73f
Alessandro Bonometti <alessandro.bonometti@massive.co>
2018-08-13 15:11:55 +0200

    Added LightBDD tests (not working)

--- CSharp/src/test/SocialNetwork.Tests.Acceptance.LightBDD/SocialNetworkTests.cs
+++ CSharp/src/test/SocialNetwork.Tests.Acceptance.LightBDD/SocialNetworkTests.cs
@@ -1,7 +1,23 @@
-namespace SocialNetwork.Tests.Acceptance.LightBDD
-{
-    public class Acceptance
-    {
-        
-    }
-}
+// using LightBDD.Core.Configuration;
+// using LightBDD.Framework.Reporting.Configuration;
+// using LightBDD.Framework.Reporting.Formatters;
+// using LightBDD.XUnit2;
+
+// //[assembly: ConfiguredLightBddScope]
+
+// namespace SocialNetwork.Tests.Acceptance.LightBDD
+// {
+//     [FeatureDescription(
+//     @"In order to fullfill my ego
+//     As a Moron
+//     I Want to posto to social Network")]
+//     public partial class SocialNetworkTests : FeatureFixture
+//     {
+//         [Scenario]
+//         public void Can_Read_Alice_Posts(){
+//             // Runner.RunScenario(
+//             //     Given_
+//             // )
+//         }
+//     }
+// }
